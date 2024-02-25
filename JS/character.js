7a33a4be8968e48abecc21121c07f25cb1995503
vemula muni varma <munimunnavemula123@gmail.com>
2024-02-25 13:42:43 +0530

    comic page added

--- JS/character.js
+++ JS/character.js
@@ -3,17 +3,18 @@ const id = urlParams.get('id');
 
 let dataLoad = '';
 const Charurl = `${baseCharactersUrl}characters/${id}?&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-const ComicUrl = `${baseCharactersUrl}characters/${id}/comics?&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+const ComicUrl = `${baseCharactersUrl}characters/${id}/comics?&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+const EventUrl = `${baseCharactersUrl}characters/${id}/events?&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+console.log(EventUrl);
 
-
-console.log(Charurl);
+//console.log(Charurl);
 fetch(Charurl).then(function (response) {
   return response.json();
 })
   .then(res => {
-    console.log(res);
+    //console.log(res);
     let data = res.data.results;
-
+    console.log(data);
     dataLoad += ` <div class="char-cover">
                         <img class="char-cover-pic" src="${data[0].thumbnail.path}.${data[0].thumbnail.extension}"/>`;
     dataLoad += `   <div class="char-content">
@@ -27,30 +28,67 @@ fetch(Charurl).then(function (response) {
                       `;
 
     document.getElementById('char-details').innerHTML += dataLoad;
-    console.log(data[0].comics.items[0].resourceURI);
   });
 
+
+let ComicsData = [];
+let comicsBool = new Map();
 fetch(ComicUrl).then(function (response) {
   return response.json();
 })
   .then(res => {
-    console.log(res);
+    //console.log(res);
     let data = res.data.results;
     let currData = '<div id="com-div">';
-
+    if(data.length>0){
     data.forEach(element => {
       currData += `<div class="com-card">
-                            <button id="fav-Char-${element.id}" class='add-fav' onclick="toggleLike(${element.id})">
-                                <i class="fa-solid fa-star fa-beat"></i>
-                            </button>
                             <img src="${element.images[0].path}.${element.images[0].extension}">
-                           <div class="com-title"><p>${element.title}</p></div>
+                           <div class="com-title"><a href="./comic.html?id=${element.id}">${element.title}</a></div>
                         </div>`
-
-      console.log(element.title);
-    });
+      if(!comicsBool.get(element.id)){
+        //console.log(element.id);
+        comicsBool.set(element.id, true);
+        ComicsData.push(element);
+      }
+      
+      localStorage.setItem('Comics', JSON.stringify(ComicsData));
+      //console.log(element.title);
+    });}
+    else{
+      currData+=`<h2>No Comics to show</h2>`;
+    }
     currData += '</div>';
+    console.log(currData);
     document.getElementById('comics').innerHTML += currData;
   });
 
 
+
+  let EventsData = [];
+ 
+  fetch(EventUrl).then(function (response) {
+    return response.json();
+  })
+    .then(res => {
+      //console.log(res);
+      let data = res.data.results;
+      let currData = '<div id="event-div">';
+  
+      data.forEach(element => {
+        currData += `<div class="event-card">
+                              <img src="${element.images[0].path}.${element.images[0].extension}">
+                             <div class="com-title"><a href="./comic.html?id=${element.id}">${element.title}</a></div>
+                          </div>`
+        if(!comicsBool.get(element.id)){
+          //console.log(element.id);
+          comicsBool.set(element.id, true);
+          ComicsData.push(element);
+        }
+        
+        localStorage.setItem('Comics', JSON.stringify(ComicsData));
+        //console.log(element.title);
+      });
+      currData += '</div>';
+      document.getElementById('comics').innerHTML += currData;
+    });
---
+++ JS/comic.js
@@ -0,0 +1,52 @@
+async function fetchCharacterData(url) {
+    return fetch(url)
+            .then(res=>res.json())
+            .then(res=>res);
+} 
+
+async function loadComicCharacter(characterItem) {
+    const characterData = await fetchCharacterData(`${characterItem.resourceURI}?&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+    let data = characterData.data.results[0];
+    console.log(data);
+    const currData = `<div class="com-card">
+                <button id="fav-Char-${data.id}" class='add-fav' onclick="toggleLike(${data.id})">
+                    <i class="fa-solid fa-star fa-beat"></i>
+                </button>
+                <img src="${data.thumbnail.path}.${data.thumbnail.extension}">
+                <div class="com-title"><a href="./character.html?id=${data.id}">${data.name}</a></div>
+            </div>`
+
+    document.getElementById('com-div').innerHTML += currData;
+}
+
+function loadComicPage() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const comicId = urlParams.get('id');
+
+    let wholeComicData = JSON.parse(localStorage.getItem('Comics'));
+
+    const Comic = wholeComicData.find(e => e.id == comicId);
+
+    let dataLoad = '';
+    dataLoad += ` <div class="char-cover">
+                        <img class="char-cover-pic" src="${Comic.images[0].path}.${Comic.images[0].extension}"/>`;
+    dataLoad += `   <div class="char-content">
+                            <h1 style="color:red">${Comic.title}</h1>
+                            <p >${Comic.description}</p>
+                            <button id="fav-button">Favorite</button>
+                        </div>
+                        
+                      </div>
+                      
+                      `;
+
+    document.getElementById('char-details').innerHTML += dataLoad;
+    //console.log(data[0].comics.items[0].resourceURI)
+
+    Comic.characters.items.forEach(character => {
+        loadComicCharacter(character)
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', loadComicPage);
